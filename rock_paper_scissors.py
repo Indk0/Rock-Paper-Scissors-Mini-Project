1b8497da7bc076741ccaa61b243e2fabd1d204c4
Indk0 <inderik302@gmail.com>
2025-02-13 20:06:55 +0000

    Add extra functionality to the entire code and end bit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,33 +13,44 @@ while True:
     print('3) ✌️')
     print('4) Quit😢')
 
-    try:
-        pl = int(input('Please pick a move:'))
+    pl = int(input('Please pick a move:'))
 
-        cpu = random.randint(1,3)
+    cpu = random.randint(1,3)
 
-        print('You chose: ', pl)
-        print('CPU chose: ', cpu)
+    print('You chose: ', pl)
+    print('CPU chose: ', cpu)
 
-        if pl == cpu:
-            print(f"Both players selected {pl}. It's a tie.")
-        elif pl == 1:
-            if cpu == 2:
-                print('CPU wins!')
-            else:
-                print('You win!')
-        elif pl == 2:
-            if cpu == 1:
-                print('CPU chose rock. You win!')
-            else:
-                print('You got chopped.😔')
-        elif pl == 3:
-            if cpu == 1:
-                print('You lose!')
-            else:
-                print('You win!')
+    if pl == cpu:
+        print(f"Both players selected {pl}. It's a tie.")
+        break
+    elif pl == 1:
+        if cpu == 2:
+            print('CPU wins!')
+            break
         else:
-            print("You quit the session, you're welcome anytime!!")
+            print('You win!')
+            break
+    elif pl == 2:
+        if cpu == 1:
+            print('CPU chose rock. You win!')
+            break
+        else:
+            print('You got chopped.😔')
+            break
+    elif pl == 3:
+        if cpu == 1:
+            print('You lose!')
+            break
+        else:
+            print('You win!')
+            break
+    elif pl == 4:
+        print("You quit the session, you're welcome anytime!!")
+        break
+    else:
+        print('Invalid input, please try again.')
 
-    except ValueError:
-        print('Invalid input, please try pick between 1 and 4.')
+play_again = input("Play again? (Y/N): ")    
+if play_again != Y and != y:
+        print('You want to touch grass? Enjoy')
+        

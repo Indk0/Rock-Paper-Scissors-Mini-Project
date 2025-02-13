c63e637ddab7d21b5c52996c833b865426aa5578
Indk0 <inderik302@gmail.com>
2025-02-13 19:17:48 +0000

    Make initial code

---
+++ rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random
+
+print('=============')
+print('Welcome to the rock, paper, scissors')
+print('=============')
+
+print('Pick from the following.')
+
+print('1) ✊')
+print('2) 🤚')
+print('3) ✌️')
+
+pm = int(input('Please pick a move:'))
+
+movran = random.randint(1,3)
+
+if movran == 1:
+
+
+elif movran == 2:
+
+elif movran == 3:
+
+else:

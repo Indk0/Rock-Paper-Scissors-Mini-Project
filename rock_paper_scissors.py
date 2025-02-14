70e25b6dc259eefda2921d98cf84cceb19aa8384
Indk0 <inderik302@gmail.com>
2025-02-14 10:08:12 +0000

    Add a loop for play again function

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,56 +1,60 @@
 import random
 
-print('=============')
-print('Welcome to the rock, paper, scissors')
-print('=============')
-
-print('Pick from the following.')
-
 while True:
+    print('=============')
+    print('Welcome to the rock, paper, scissors')
+    print('=============')
 
-    print('1) ✊')
-    print('2) 🤚')
-    print('3) ✌️')
-    print('4) Quit😢')
+    print('Pick from the following.')
 
-    pl = int(input('Please pick a move:'))
+    while True:
+        print('1) ✊')
+        print('2) 🤚')
+        print('3) ✌️')
+        print('4) Quit😢')
 
-    cpu = random.randint(1,3)
+        try:
+            pl = int(input('Please pick a move:'))
 
-    print('You chose: ', pl)
-    print('CPU chose: ', cpu)
+            cpu = random.randint(1,3)
 
-    if pl == cpu:
-        print(f"Both players selected {pl}. It's a tie.")
-        break
-    elif pl == 1:
-        if cpu == 2:
-            print('CPU wins!')
-            break
-        else:
-            print('You win!')
-            break
-    elif pl == 2:
-        if cpu == 1:
-            print('CPU chose rock. You win!')
-            break
-        else:
-            print('You got chopped.😔')
-            break
-    elif pl == 3:
-        if cpu == 1:
-            print('You lose!')
-            break
-        else:
-            print('You win!')
-            break
-    elif pl == 4:
-        print("You quit the session, you're welcome anytime!!")
-        break
-    else:
-        print('Invalid input, please try again.')
+            print('You chose: ', pl)
+            print('CPU chose: ', cpu)
 
-play_again = input("Play again? (Y/N): ")    
-if play_again != Y and != y:
-        print('You want to touch grass? Enjoy')
+            if pl == cpu:
+                print(f"Both players selected {pl}. It's a tie.")
+                break
+            elif pl == 1:
+                if cpu == 2:
+                    print('CPU wins!')
+                    break
+                else:
+                    print('You win!')
+                    break
+            elif pl == 2:
+                if cpu == 1:
+                    print('CPU chose rock. You win!')
+                    break
+                else:
+                    print('You got chopped.😔')
+                    break
+            elif pl == 3:
+                if cpu == 1:
+                    print('You lose!')
+                    break
+                else:
+                    print('You win!')
+                    break
+            else:
+                print("You quit the session, you're welcome anytime!!")
+                break
+            
+        except ValueError:
+                print('Invalid input, please try again.')
+                break
+
+    play_again = input("Play again? (Y/N): ")    
+    if play_again != "Y" and play_again.lower() != "y":
+            print('You want to touch grass? Enjoy 🏡')
+            break
         
